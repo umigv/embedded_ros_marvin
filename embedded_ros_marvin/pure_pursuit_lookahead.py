8b89704afc1854b3cfc6732aa2113950367d87a1
John Rose <johnrose@umich.edu>
2025-04-15 21:34:14 -0400

    Created action implementation

--- embedded_ros_marvin/pure_pursuit_lookahead.py
+++ embedded_ros_marvin/pure_pursuit_lookahead.py
@@ -1,10 +1,15 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.action import ActionServer
 from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
 from infra_interfaces.action import FollowPath  # Import the action
 import math
+import threading
+from scipy.spatial.transform import Rotation
+
 
 
 class PurePursuitNode(Node):
@@ -17,49 +22,94 @@ class PurePursuitNode(Node):
         self.lookahead_distance = 0.3
 
         # State
-        self.path = []
         self.pose = None
-        self.reached_goal = False
-        self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.pose_lock = threading.Lock()
+        self.action_complete_cv = threading.Condition()
+        self.control_timer = None   
+        self.reset_action_state()
 
+        self.cb_group = ReentrantCallbackGroup()
+        
+        self.create_subscription(Odometry, '/odom', self.odom_callback, 10, callback_group=self.cb_group)
         self.cmd_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-
         self.action_server = ActionServer(
             self,
             FollowPath,
             'follow_path',
-            execute_callback=self.execute_callback
-        )  # Action server
+            execute_callback=self.execute_follow_path,
+            callback_group=self.cb_group
+        )
 
-        self.create_timer(0.1, self.control_loop)
+    def reset_action_state(self):
+        self.path = []
+        self.reached_goal = False
+        if self.control_timer:
+            self.control_timer.cancel()
+            self.control_timer = None
 
-    def execute_callback(self, goal_handle):
-        self.get_logger().info('Received a new path from action client.')
-        self.path = [(p.x, p.y) for p in goal_handle.request.path]  # Update path
-        while not self.reached_goal:
-            rclpy.spin_once(self)
-            if self.reached_goal:
-                break
+    def execute_follow_path(self, goal_handle):
+        self.path = [(p.x, p.y) for p in goal_handle.request.path]
+        self.get_logger().info(f'Received a new path from action client: {self.path}')
+        self.control_timer = self.create_timer(0.1, self.control_loop, callback_group=self.cb_group)
+        with self.action_complete_cv:
+            while not self.reached_goal:
+                self.action_complete_cv.wait()
+        self.reset_action_state()
         goal_handle.succeed()
         result = FollowPath.Result()
+        result.success = True
+        self.get_logger().info('FollowPath action completed')
         return result
 
+    def control_loop(self):
+        local_point = self.find_lookahead_point()
+
+        if local_point is None:
+            self.cmd_pub.publish(Twist())  # Stop
+            if  self.reached_goal:
+                self.get_logger().info('Path completed.')
+                with self.action_complete_cv:
+                    self.action_complete_cv.notify_all()
+            else:
+                self.get_logger().info('Waiting for vaild lookahead point.')
+            return
+
+        self.get_logger().info(f'Lookahead point: {local_point}')
+        local_x, local_y = local_point
+        curvature = 2 * local_y / (local_x**2 + local_y**2)
+        dist = math.hypot(local_x, local_y)
+
+        linear = min(self.max_linear_speed, dist)
+        angular = max(-self.max_angular_speed, min(self.max_angular_speed, linear * curvature))
+
+        cmd = Twist()
+        cmd.linear.x = linear
+        cmd.angular.z = angular
+        self.cmd_pub.publish(cmd) 
+
     def odom_callback(self, msg):
-        pos = msg.pose.pose.position
-        ori = msg.pose.pose.orientation
-        yaw = self.get_yaw_from_quaternion(ori)
-        self.pose = (pos.x, pos.y, yaw)
+        # self.get_logger().info('Received odometry data')
+        with self.pose_lock:
+            pos = msg.pose.pose.position
+            ori = msg.pose.pose.orientation
+            yaw = self.get_yaw_from_quaternion(ori)
+            # yaw *= -1
+            self.pose = (pos.x, pos.y, yaw)
+            # self.get_logger().info(f'{self.pose}')
 
     def get_yaw_from_quaternion(self, q):
-        siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
-        cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
-        return math.atan2(siny_cosp, cosy_cosp)
+        rot = Rotation.from_quat([q.x, q.y, q.z, q.w])
+        roll, pitch, yaw = rot.as_euler('xyz', degrees=False)
+
+        # siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
+        # cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
+        return yaw
 
     def find_lookahead_point(self):
-        if self.pose is None or not self.path:
-            return None
-
-        x, y, yaw = self.pose
+        with self.pose_lock:
+            if not self.pose: 
+                return
+            x, y, yaw = self.pose
 
         for gx, gy in self.path:
             dx = gx - x
@@ -71,47 +121,24 @@ class PurePursuitNode(Node):
             dist = math.hypot(local_x, local_y)
 
             # Prevents driving backwards or directly to the side
-            if local_x > 0.05 and dist >= self.lookahead_distance:
+            if dist >= self.lookahead_distance:
                 return local_x, local_y
 
         self.reached_goal = True
         return None
 
-    def control_loop(self):
-        self.get_logger().info('Control loop running.')
-        if self.pose is None:
-            return
-
-        local_point = self.find_lookahead_point()
-
-        if local_point is None:
-            self.cmd_pub.publish(Twist())  # Stop
-            if  self.reached_goal:
-                self.get_logger().info('Path completed.')
-            else:
-                self.get_logger().info('Waiting for vaild lookahead point.')
-            return
-
-        local_x, local_y = local_point
-        curvature = 2 * local_y / (local_x**2 + local_y**2)
-        dist = math.hypot(local_x, local_y)
-
-        linear = min(self.max_linear_speed, dist)
-        angular = max(-self.max_angular_speed, min(self.max_angular_speed, linear * curvature))
-
-        cmd = Twist()
-        cmd.linear.x = linear
-        cmd.angular.z = angular
-        self.cmd_pub.publish(cmd)
-
-
 def main(args=None):
     rclpy.init(args=args)
     node = PurePursuitNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
 
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    
+    try:
+        executor.spin()
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
