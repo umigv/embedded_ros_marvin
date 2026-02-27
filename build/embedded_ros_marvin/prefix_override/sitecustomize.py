e163761a23e3875b56816d19862e77dd42cb7d27
hannahh3853 <hannerd@umich.edu>
2026-02-27 00:54:49 +0000

    fixed state machine with nav

--- build/embedded_ros_marvin/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ubuntu/complete_embedded_ws/embedded_ros_marvin/install/embedded_ros_marvin'
