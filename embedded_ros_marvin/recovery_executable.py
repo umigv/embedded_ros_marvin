3d32176781957c12914c3c4f518f8306b162987a
hannahh3853 <hannerd@umich.edu>
2026-04-14 03:03:55 +0000

    updated recovery behavior, untested

--- embedded_ros_marvin/recovery_executable.py
+++ embedded_ros_marvin/recovery_executable.py
@@ -13,20 +13,22 @@ from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDur
 from statistics import median
 
 recovery_executable = None
-ANGULAR_VELOCITY = 0.3 #radians per second, which is about 57 degrees per second
-THRESHOLD_DISTANCE = 60 #centimeters
-BACKUP_AFTER_SWEEP_VELOCITY = 0.1 #m/s
+ANGULAR_VELOCITY = 0.3 #radians per second
+MAX_ANGULAR_SWEEP_DISTANCE = 0.349066 #radians
+THRESHOLD_DISTANCE = 60 #centimeters, distance needed to start backing up
+MAX_BACKUP_DISTANCE = 0.50 #meters
+BACKUP_ALLOWANCE = 15 #centimeters, if the ultrasound reader reads less than this, the robot stops backing up and ends recovery
+PROPORTIONAL = 0.333 #the p term for the fake p loop 
 
 class RecoveryExecutable(Node):
     def __init__(self):
         super().__init__('recovery_executable')
         self.publisher_Twist = self.create_publisher(Twist, 'recovery_cmd_vel', 10)
-        timer_period = 0.5  # seconds
-        self.velocity_publishing_timer = self.create_timer(timer_period, self.velocity_publishing)
-        self.back_up_velocity_timer = self.create_timer(timer_period, self.set_velocity_from_error)
-        self.sweep_timer = self.create_timer(timer_period, self.sweep_left_and_right)
+        self.timer_period = 0.5  # seconds
+        self.velocity_publishing_timer = self.create_timer(self.timer_period, self.velocity_publishing)
+        self.back_up_velocity_timer = self.create_timer(self.timer_period, self.set_velocity_from_error)
+        self.sweep_timer = self.create_timer(self.timer_period, self.sweep_left_and_right)
         self.arduino_timer = self.create_timer(0.05, self.updateArduinoReading)
-
 
         self.subscription = self.create_subscription(String, 'state', self.listener_callback, QoSProfile(
                 history=QoSHistoryPolicy.KEEP_LAST,
@@ -35,13 +37,11 @@ class RecoveryExecutable(Node):
                 durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
             ),)
 
-        self.ultraSoundReadingFloat = 2000.0
-        self.ultraSoundReadingHistory = []  # Store last 5 readings for median filter
-        self.targetLinearVelocity = 0.0 #m/s???
-        self.targetAngularVelocity = 0.0 #rad/s???
-        self.targetPosition = 0.50 #meters
+        self.ultrasoundReading = 2000.0
+        self.ultrasoundReadingHistory = []  # Store last 5 readings for median filter
+        self.targetLinearVelocity = 0.0 #m/s
+        self.targetAngularVelocity = 0.0 #rad/s
         self.distanceTraveled = 0.0
-        self.proportional = 0.333
         self.radiansTravelled = 0.0
         self.state = "NoPublishing"
        
@@ -57,7 +57,7 @@ class RecoveryExecutable(Node):
             self.get_logger().info('service not available, waiting again...')
         self.req = SetBool.Request()
 
-    #This gets in the arduino reading and updates the self.ultraSoundReadingFloat, which is where we use the ultrasound readigns in the rest of the code
+    #This gets in the arduino reading and updates the self.ultrasoundReading, which is where we use the ultrasound readigns in the rest of the code
     def updateArduinoReading(self):
         if self.arduino is None:
             return
@@ -68,13 +68,10 @@ class RecoveryExecutable(Node):
                 ultraSoundReadingString = packet.decode('utf-8', errors='ignore').rstrip('\n')
                 try:
                     raw_reading = float(ultraSoundReadingString)
-                    self.ultraSoundReadingHistory.append(raw_reading)
-                    if len(self.ultraSoundReadingHistory) > 5:
-                        self.ultraSoundReadingHistory.pop(0)
-                    self.ultraSoundReadingFloat = median(self.ultraSoundReadingHistory)
-                    if self.state == "BeginRecovery":
-                        self.state = "temp"
-                        self.begin_recovery()
+                    self.ultrasoundReadingHistory.append(raw_reading)
+                    if len(self.ultrasoundReadingHistory) > 5:
+                        self.ultrasoundReadingHistory.pop(0)
+                    self.ultrasoundReading = median(self.ultrasoundReadingHistory)
                 except:
                     self.get_logger().info("not reading ultrasound values")
 
@@ -85,74 +82,71 @@ class RecoveryExecutable(Node):
                 self.get_logger().info("Calling end_recovery due to Arduino disconnect")
                 self.end_recovery()
                 
-    def listener_callback(self, msg): #Note from Hannah & Ash: How does msg variable work, and does it conflict with msg var in twist?
+    def listener_callback(self, msg): 
         if msg.data == 'recovery':
-            self.state = "BeginRecovery"
+            self.begin_recovery()
 
     #This funciton is called periodically. It actually publishes the velocity messages and has the logic of whether to call the function that just drives teh robot backwards or call the sweep function
     def velocity_publishing(self):
-        #self.get_logger().info(f'calling timer callback')
-        self.get_logger().info(f'ultrasoundreading: {self.ultraSoundReadingFloat}')
+        self.get_logger().info(f'ultrasoundreading: {self.ultrasoundReading}')
 
     #this is where the velocity messages are actually published
         msg = Twist()
         msg.linear.x = -self.targetLinearVelocity
         msg.angular.z = self.targetAngularVelocity
-        #self.get_logger().info(str(msg))
         if self.state != "NoPublishing":
             self.publisher_Twist.publish(msg)
-        #self.get_logger().info(f'ultrasoundreading: {self.ultraSoundReadingFloat}')
 
     #this function sets the velocity to have the robot back up. The velocity starts large, then decreases as the robot gets closer to its target
-    #currently, it is set to backup 0.30 meters
+    #not a real pid loop, distance traveled is calculated, not read in from sensors
     def set_velocity_from_error(self):
-
-        if self.state == "beginBackUp":
+        if self.state == "beginBackUp" and self.ultrasoundReading > BACKUP_ALLOWANCE:
             self.get_logger().info("backing up")
-            self.distanceTraveled = self.distanceTraveled + (0.5 * self.targetLinearVelocity)
-            error = self.targetPosition - self.distanceTraveled
-            self.targetLinearVelocity = self.proportional * error
-            if error < 0.01:
+            #calculates how far we have traveled from our speed and the frequency this function is called at
+            self.distanceTraveled = self.distanceTraveled + (self.timer_period * self.targetLinearVelocity)
+            error = MAX_BACKUP_DISTANCE - self.distanceTraveled
+            self.targetLinearVelocity = PROPORTIONAL * error
+            #Because this is a calculated error, I don't think this error allowance needs to be tuned very much
+            if error < 0.01: 
                 self.end_recovery()
-            self.get_logger().info(f'distancetraveled: {self.distanceTraveled}')
+            self.get_logger().info(f' calculated distancetraveled: {self.distanceTraveled}')
             #self.get_logger().info(f'error: {error}')
-            self.get_logger().info(f'targetLinearVelocity: {self.targetLinearVelocity}')
+            self.get_logger().info(f'targetLinearVelocity: {self.targetLinearVelocity}')  
+        
+        #If the ultrasound reader reads less than the backup allowance, it stops backing up and ends recovery
+        elif self.state == "beginBackUp" and self.ultrasoundReading <= BACKUP_ALLOWANCE:
+            self.get_logger().info(f'saw object within backup allowance, ending backup')
+            self.end_recovery()  
 
 #this function sets the robot's angular velocity to sweep left and right if it senses something directly behind it
-#the logic in the timer callback function should stop it from running once it doesn't sense something behind it
     def sweep_left_and_right(self):
-        #self.get_logger().info(f'sweeping function called')
-        if self.state != "beginSweep" and self.state != "flipDirectionSweep" and self.state != "backUpAfterSweep":
+        if self.state != "beginSweep" and self.state != "flipDirectionSweep":
             return
-        if self.ultraSoundReadingFloat >= THRESHOLD_DISTANCE:
+        
+        #if the ultrasound sensor is reading more than the threshold distance, start backing up
+        if self.ultrasoundReading >= THRESHOLD_DISTANCE:
             self.targetAngularVelocity = 0.0
             self.state = "beginBackUp"
         elif self.state == "beginSweep":
             self.get_logger().info(f'sweeping counterclockwise')
-            if self.radiansTravelled < 0.349066:#sweeps 20 degrees one direction
-            #sweeps left pi/2 radians or 90 degrees one way at a speed of pi/9 radians per second
+            #turn one direction until radians travelled exceeds the max distance, then set state to flip direction
+            if self.radiansTravelled < MAX_ANGULAR_SWEEP_DISTANCE:
                 self.targetAngularVelocity = ANGULAR_VELOCITY
                 self.radiansTravelled = self.radiansTravelled + (self.targetAngularVelocity * 0.5)
             else: 
                 self.state = "flipDirectionSweep"
         elif self.state == "flipDirectionSweep":
-            if self.radiansTravelled > -0.349066:
+            #turn the other direction until radians traveled, is greater than max distance
+            if self.radiansTravelled > -MAX_ANGULAR_SWEEP_DISTANCE:
                 self.get_logger().info(f'sweeping clockwise')
-
                 self.targetAngularVelocity = -1 * ANGULAR_VELOCITY
                 self.radiansTravelled = self.radiansTravelled + (self.targetAngularVelocity * 0.5)
+            #If we have reached the max angular distance allowed in both directions without finding a point with large clearance, start backing up. 
+            # backup will continue until the reading is less than the backup allowance
             else:
-                self.state = "backUpAfterSweep"
                 self.targetAngularVelocity = 0.0
+                self.state = "beginBackUp"
                 self.get_logger().info(f'sweeping ended')
-
-        if self.state ==  "backUpAfterSweep":
-            if(self.ultraSoundReadingFloat > 10):
-                self.get_logger().info(f'backing up within 10 centimeters of obstacle')
-                self.targetLinearVelocity = BACKUP_AFTER_SWEEP_VELOCITY #sets velocity
-                self.targetAngularVelocity = 0.0
-            else:
-                self.end_recovery()
 
     def end_recovery(self):
         self.targetAngularVelocity = 0.0
@@ -164,14 +158,11 @@ class RecoveryExecutable(Node):
         msg.angular.z = 0.0
         self.publisher_Twist.publish(msg)
         self.state = "NoPublishing"
-        # boolmsg = Bool()
-        # boolmsg.data = False
         self.get_logger().info(f'recovery ended')
         response = self.send_request(False)
-        # self.publisher_Boolean.publish(boolmsg)
 
     def begin_recovery(self):
-        if self.ultraSoundReadingFloat >= THRESHOLD_DISTANCE:
+        if self.ultrasoundReading >= THRESHOLD_DISTANCE:
             self.state = "beginBackUp"
             self.targetAngularVelocity = 0.0
 
